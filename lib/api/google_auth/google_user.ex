dd8dcdcb196c83c81b2357a29df07a8d67f03d64
KJ Sam <samkj.ks@gmail.com>
2021-11-25 14:07:28 +0800

    Add missing "?"

--- lib/api/google_auth/google_user.ex
+++ lib/api/google_auth/google_user.ex
@@ -7,7 +7,7 @@ defmodule API.GoogleUser do
   typedstruct do
     field :id, binary(), enforce: true
     field :email_address, binary(), enforce: true
-    field :email_address_verified, boolean(), enforce: true
+    field :email_address_verified?, boolean(), enforce: true
     field :name, binary(), enforce: true
   end
 end
