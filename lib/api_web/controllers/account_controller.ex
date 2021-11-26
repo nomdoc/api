4af82bfc1e5dbdb148dbb44b3b1d225bcaea67e7
KJ Sam <samkj.ks@gmail.com>
2021-11-26 11:21:52 +0800

    Protect and rate limit account registration endpoint

--- lib/api_web/controllers/account_controller.ex
+++ lib/api_web/controllers/account_controller.ex
@@ -3,15 +3,16 @@ defmodule APIWeb.AccountController do
 
   use APIWeb, :controller
 
-  import API.Pwned
-
   alias API.Auth
+  alias API.Pwned
+  alias API.RateLimiter
+  alias API.Recaptcha
   alias API.Regex
 
   action_fallback APIWeb.FallbackController
 
   def register(%Plug.Conn{} = conn, data) do
-    types = %{email_address: :string, password: :string}
+    types = %{email_address: :string, password: :string, recaptcha_token: :string}
     params = Map.keys(types)
 
     {%{}, types}
@@ -19,17 +20,23 @@ defmodule APIWeb.AccountController do
     |> validate_required(params, message: "Please fill in required fields.")
     |> parse_string(:email_address, scrub: :all, transform: :downcase)
     |> parse_string(:password, scrub: :all)
+    |> parse_string(:recaptcha_token, scrub: :all)
     |> validate_length(:password, min: 12, message: password_invalid_message())
-    |> validate_refutation(:password, &password_breached?/1, message: password_breached_message())
+    |> validate_refutation(:password, &Pwned.password_breached?/1,
+      message: password_breached_message()
+    )
     |> validate_format(:password, Regex.password(), message: password_invalid_message())
     |> validate_email_address(:email_address)
     |> case do
       %Changeset{valid?: true} = changeset ->
-        %{email_address: email_address, password: password} = apply_changes(changeset)
+        %{email_address: email_address, password: password, recaptcha_token: recaptcha_token} =
+          apply_changes(changeset)
 
-        # TODO recaptcha
-        # TODO rate limit
-        with {:ok, _user} <- Auth.register_with_password(email_address, password),
+        ip_address = conn.remote_ip |> Tuple.to_list() |> Enum.join(".")
+
+        with :ok <- Recaptcha.check_assessment(recaptcha_token, "register_account"),
+             :ok <- RateLimiter.check_rate("register_account:#{ip_address}", 60 * 1_000, 4),
+             {:ok, _user} <- Auth.register_with_password(email_address, password),
              do: render(conn, "register.json")
 
       changeset ->
