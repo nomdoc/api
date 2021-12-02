32fef27f1e591ae939a469416a0dadbff5f06b8d
KJ Sam <samkj.ks@gmail.com>
2021-12-02 11:01:47 +0800

    Fix test

--- lib/api/hiring/recruiters_test.exs
+++ lib/api/hiring/recruiters_test.exs
@@ -10,11 +10,11 @@ defmodule API.RecruitersTest do
     test "creates a recruiter if not exists." do
       assert {:ok, %Recruiter{} = recruiter} = Recruiters.get_recruiter("user_123")
 
-      recruiter = Repo.preload(recruiter, [:job_posts])
+      recruiter = Repo.preload(recruiter, [:jobs])
 
       assert recruiter.new?
       assert recruiter.entity_id == "user_123"
-      assert Enum.empty?(recruiter.job_posts)
+      assert Enum.empty?(recruiter.jobs)
     end
 
     test "returns the user if user has already been created." do
